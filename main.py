bf177af75e3582f35a3624886159b975a6817c87
Vosapgogo <saha10t2007@gmail.com>
2026-09-21 12:58:28 +0200

    refactor(main): set app font and enable high-DPI attributes

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import sys
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QFont
 from pages.app import App
+from pages.constants import APP_FONT
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)  # ← завжди створюємо першим
-    window = App()                # ← App сам перевіряє сесію в _startup()
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+    app = QApplication(sys.argv)
+
+    app.setFont(QFont(APP_FONT, 11))  
+
+    window = App()                
     window.show()
     sys.exit(app.exec())
